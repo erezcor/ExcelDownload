5646ea73f9c05056cc5cd4eef8cafcfe284114ee
Erez Corech <erezcorech21@gmail.com>
2020-05-26 18:32:25 +0300

    downloads folder path now supports other system(besides mine)

--- src/main/java/constants/SystemConstants.java
+++ src/main/java/constants/SystemConstants.java
@@ -1,5 +1,5 @@
 package constants;
 
 public class SystemConstants {
-    public static String DOWNLOAD_FOLDER_PATH = "C:\\Users\\Erez Corech\\Downloads\\";
+    public static String DOWNLOAD_FOLDER_PATH = System.getProperty("user.home") + "\\Downloads\\";
 }
