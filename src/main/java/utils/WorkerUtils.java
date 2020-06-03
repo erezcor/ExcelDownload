e8b96f8f35fefaaf0179b0a2ea2dea95b26a7658
Erez Corech <erezcorech21@gmail.com>
2020-06-03 15:33:34 +0300

    WorkerUtils: getWorkerFromListByID function

---
+++ src/main/java/utils/WorkerUtils.java
@@ -0,0 +1,12 @@
+package utils;
+
+import entities.Worker;
+
+import java.util.List;
+
+public class WorkerUtils {
+    public static Worker getWorkerFromListByID(String id, List<Worker> workersList) {
+        return workersList.stream().filter(worker -> worker.getId().equals(id))
+                .findAny().orElseThrow();
+    }
+}
