d968f50ff5db5da7dadb9416489465093823dbf1
Erez Corech <erezcorech21@gmail.com>
2020-06-03 13:15:36 +0300

    New ExcelUtils  Functions, new iteratorToStream function

--- src/main/java/utils/ExcelUtils.java
+++ src/main/java/utils/ExcelUtils.java
@@ -3,8 +3,10 @@ package utils;
 import entities.Worker;
 import exceptions.TimeoutException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -15,33 +17,44 @@ import java.util.List;
 
 import static constants.SystemConstants.DOWNLOADS_DIRECTORY_PATH;
 import static java.lang.System.currentTimeMillis;
+import static java.lang.Thread.sleep;
 import static java.util.stream.Collectors.toList;
 import static utils.FileUtils.getLatestFileFromFolderByExtension;
 import static utils.FileUtils.getNumberOfFilesByExtensionFrom;
+import static utils.StreamUtils.getStreamFromIterator;
 import static utils.TimeoutUtils.isMaximumTimeInFunctionPassed;
 
 public class ExcelUtils {
-    private static int MILLISECONDS_TO_WAIT_BETWEEN_EACH_LOOP = 500;
+    private static int WAIT_MILLISECONDS_WHEN_EXCEL_NOT_DOWNLOADED = 500;
     private final static int FIRST_SHEET_INDEX = 0;
     private static int HEADLINES_ROW_INDEX = 0;
     private final static String excelFileExtension = "xlsx";
 
     public static List<List<String>> getExcelFileAsStringLists(File file) throws IOException, InvalidFormatException {
-        List<List<String>> excelTable = new ArrayList<>();
-        DataFormatter formatter = new DataFormatter();
-        XSSFSheet excelSheet = new XSSFWorkbook(file).getSheetAt(FIRST_SHEET_INDEX);
+        return getDataFromExcelSheet(new XSSFWorkbook(file).getSheetAt(FIRST_SHEET_INDEX));
+        // todo make function to get last element from list
+        // Todo if use stream make a util convert iterator to stream
+    }
 
-        int lastColumnIndex = excelSheet.getRow(HEADLINES_ROW_INDEX).getLastCellNum() - 1;
+    private static List<List<String>> getDataFromExcelSheet(XSSFSheet excelSheet) {
+        return getStreamFromIterator(excelSheet.rowIterator())
+                .map(ExcelUtils::getCellStringsFromRow).collect(toList());
+    }
 
-        for (Row row : excelSheet) {
-            excelTable.add(new ArrayList<>());
-            for (int cellIndex = 0; cellIndex <= lastColumnIndex; cellIndex++) {
-                String cellAsString = formatter.formatCellValue(row.getCell(cellIndex));
-                excelTable.get(row.getRowNum()).add(cellAsString);
-            }
+    private static List<String> getCellStringsFromRow(Row row) {
+        List<String> excelRowCells = new ArrayList<>();;
+        for (int cellIndex = 0; cellIndex <= getLastColumnIndex(row.getSheet()); cellIndex++) {
+            excelRowCells.add(getStringFromCell(row.getCell(cellIndex)));
         }
+        return excelRowCells;
+    }
 
-        return excelTable;
+    private static String getStringFromCell(Cell cell) {
+        return new DataFormatter().formatCellValue(cell);
+    }
+
+    private static int getLastColumnIndex(Sheet excelSheet) {
+        return excelSheet.getRow(HEADLINES_ROW_INDEX).getLastCellNum() - 1;
     }
 
     public static List<Worker> getExcelFileAsWorkersList(File file) throws IOException, InvalidFormatException {
@@ -57,7 +70,7 @@ public class ExcelUtils {
         long timeWhenEnteredFunctionInMillis = currentTimeMillis();
 
         while (!isExcelFileDownloadFinished(numberOfExcelFilesInDownloadsDirectoryBeforeDownload)) {
-            Thread.sleep(MILLISECONDS_TO_WAIT_BETWEEN_EACH_LOOP);
+            sleep(WAIT_MILLISECONDS_WHEN_EXCEL_NOT_DOWNLOADED);
 
             if (isMaximumTimeInFunctionPassed(timeWhenEnteredFunctionInMillis, maximumSecondsToWait)) {
                 throw new TimeoutException("Maximum time timed out in waitUntilExcelFileIsDownloaded");
