00e36a2c8848677803277b57fba3cd1f2cbbbffb
Erez Corech <erezcorech21@gmail.com>
2020-06-03 16:36:32 +0300

    cosmetic change

--- src/main/java/utils/WorkerUtils.java
+++ src/main/java/utils/WorkerUtils.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public class WorkerUtils {
     public static Worker getWorkerFromListByID(String id, List<Worker> workersList) {
-        return workersList.stream().filter(worker -> worker.getId().equals(id))
-                .findAny().orElseThrow();
+        return workersList.stream().filter(worker -> worker.getId().equals(id)).findAny().orElseThrow();
     }
 }
